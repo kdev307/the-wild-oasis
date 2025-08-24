c57e038ff01007e7ed96a8129386dd5acce95080
kdev307 <127479436+kdev307@users.noreply.github.com>
2025-08-24 16:24:56 +0530

    some UI corrections

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -4,6 +4,7 @@ import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
+import Button from "../../ui/Button";
 
 const TableRow = styled.div`
     display: grid;
@@ -66,9 +67,9 @@ function CabinRow({ cabin }) {
             <div>Fits up to {maxCapacity} guests</div>
             <Price>{formatCurrency(regularPrice)}</Price>
             <Discount>{formatCurrency(discount)}</Discount>
-            <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
+            <Button onClick={() => mutate(cabinId)} disabled={isDeleting}>
                 Delete
-            </button>
+            </Button>
         </TableRow>
     );
 }
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -52,13 +52,13 @@ const Button = styled.button`
     border: none;
     border-radius: var(--border-radius-sm);
     box-shadow: var(--shadow-sm);
-    ${(props) => sizes[props.sizes]};
-    ${(props) => variations[props.variations]}
+    ${(props) => sizes[props.size]};
+    ${(props) => variations[props.variation]}
 `;
 
 Button.defaultProps = {
-    variations: variations.primary,
-    size: sizes.medium,
+    variation: "primary",
+    size: "medium",
 };
 
 export default Button;
