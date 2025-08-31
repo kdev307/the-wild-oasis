5ff05134b60a384f3bcda90901b39f71ae526aa0
kdev307 <127479436+kdev307@users.noreply.github.com>
2025-08-31 16:10:18 +0530

    created bookings table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -5,6 +5,7 @@ import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
     const { isLoading, cabins } = useCabins();
@@ -25,6 +26,8 @@ function CabinTable() {
     const [field, direction] = sortBy.split("-");
     const modifier = direction === "asc" ? 1 : -1;
     const sortedCabins = filteredCabins.sort((a, b) => (a[field] - b[field]) * modifier);
+
+    if (!cabins.length) return <Empty resourceName="cabins" />;
 
     return (
         <Menus>
