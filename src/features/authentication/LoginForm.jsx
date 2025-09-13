55b8ebfc3b3fdc276adfcc21d8157265c973c190
kdev307 <127479436+kdev307@users.noreply.github.com>
2025-09-13 14:28:04 +0530

    fixing some minor bugs

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from "../../ui/SpinnerMini";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-    const [email, setEmail] = useState("john.doe@example.com");
-    const [password, setPassword] = useState("Test@1234");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const { login, isLoading } = useLogin();
 
     function handleSubmit(e) {
